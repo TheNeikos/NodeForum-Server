73b0afd96871b7ff1825f31cbba02449190be2c9
Marcel Mueller <com2040@gmail.com>
2014-09-10 15:30:13 +0200

    Added User.login method

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,8 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     BaseSchema = require('./basenode').base_schema,
-    BaseNode = mongoose.model('BaseNode');
+    BaseNode = mongoose.model('BaseNode'),
+    bcrypt = require('bcrypt');
 
 var UserSchema = new BaseSchema({
     email: String,
@@ -13,6 +14,19 @@ var UserSchema = new BaseSchema({
 UserSchema.path('email').validate(function(value) {
     return /.+@.+\..+/.test(value);
 });
+
+UserSchema.statics.login = function(email, password, cb) {
+    this.findOne({email: email}, function(err, user){
+        if (err) return cb(err);
+        if (!user) return cb("User not found.");
+
+        bcrypt.compare(password, user.passwordHash, function(err, res) {
+            if (err) return cb(err);
+            if (!res) return cb("User not found.");
+            cb(undefined, user);
+        });
+    });
+}
 
 
 var User = BaseNode.discriminator('User', UserSchema);
